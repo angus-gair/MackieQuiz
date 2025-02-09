68883023132d09f0c08b8d19b1434389684acc17
angus11 <36769064-angus11@users.noreply.replit.com>
2025-02-09 22:01:21 +0000

    Agent query: Could you check if the admin button now shows 'Admin' instead of 'Manage Questions'?
    Update button label to "Admin" in home page.
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/086f901d-cd0b-4082-98e5-74b11100b9dc/c34469b7-3d6a-4c2d-bb79-3b03b0f80aaf.jpg

--- client/src/pages/home-page.tsx
+++ client/src/pages/home-page.tsx
@@ -211,7 +211,7 @@ export default function HomePage() {
                   )}
                 >
                   <Plus className="mr-2 h-4 w-4" />
-                  Manage Questions
+                  Admin
                 </Button>
               </Link>
             )}
